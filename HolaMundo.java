f0988c5fc782c927e3791f9c311e657d9886b89f
Leonila <tauro25_abril@live.com.mx>
2015-10-27 16:47:30 -0600

    HolaMundo.java
    Trabajo realizado para la materia de Proyectos de Tecnologías de la información.

---
+++ HolaMundo.java
@@ -0,0 +1,8 @@
+import javax.swing.JOptionPane;
+// Hola Mundo !!!
+// Leonila, trabajo para la materia Proyectos
+public class MiClase {
+  public static void(String[] args) {
+    JOptionPane.showMessageDialog(null,"Hola Mundo,LEO","My first program",JOptionPane.QUESTION_MESSAGE);
+  }
+}
